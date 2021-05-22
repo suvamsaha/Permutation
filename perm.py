03cf2f369b3e93ac3a8e6e39943c4c3f8a5480d9
suvam saha <45495208+suvamsaha@users.noreply.github.com>
2021-05-22 21:32:34 +0530

    Update perm.py

--- perm.py
+++ perm.py
@@ -8,3 +8,30 @@ def permutation(data):
 
 
 print(permutation('abc'))
+
+def permutation(data):
+    s = list()
+    if 1 == len(data):
+        return data
+    for i in range(len(data)):
+        l = permutation(data[:i]+data[i+1:])
+        for x in l:
+            s.append(data[i] + x)
+    return s
+
+
+#print(permutation('abcd'))
+
+
+def combination(data):
+    s = list()
+    if 1 == len(data):
+        return [data]
+    for i in range(len(data)):
+        l = combination(data[:i]) + combination(data[i + 1:])
+        for x in l:
+            s.append(x)
+            s.append(data[i] + x)
+    return s
+
+print(set(combination('ababccc')))
